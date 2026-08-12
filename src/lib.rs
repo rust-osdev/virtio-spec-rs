ef15ca43918c9c9e962186ea30a87cf66ba2f5f7
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2026-08-12 10:58:56 +0200

    refactor: sort `DeviceStatus` values according to spec 1.4

--- src/lib.rs
+++ src/lib.rs
@@ -148,23 +148,23 @@ virtio_bitflags! {
         /// </div>
         const DRIVER = 2;
 
-        /// Indicates that something went wrong in the guest,
-        /// and it has given up on the device. This could be an internal
-        /// error, or the driver didn't like the device for some reason, or
-        /// even a fatal error during device operation.
-        const FAILED = 128;
+        /// Indicates that the driver is set up and ready to
+        /// drive the device.
+        const DRIVER_OK = 4;
 
         /// Indicates that the driver has acknowledged all the
         /// features it understands, and feature negotiation is complete.
         const FEATURES_OK = 8;
 
-        /// Indicates that the driver is set up and ready to
-        /// drive the device.
-        const DRIVER_OK = 4;
-
         /// Indicates that the device has experienced
         /// an error from which it can't recover.
         const DEVICE_NEEDS_RESET = 64;
+
+        /// Indicates that something went wrong in the guest,
+        /// and it has given up on the device. This could be an internal
+        /// error, or the driver didn't like the device for some reason, or
+        /// even a fatal error during device operation.
+        const FAILED = 128;
     }
 }
 
