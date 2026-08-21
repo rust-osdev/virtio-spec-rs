9532c94822b508e2fc4c0cfb89de8b8b7ef117a5
Berk Erdem <berk.erdem@rwth-aachen.de>
2026-08-21 08:25:45 +0200

    feat: add virtio-blk definitions
    Co-authored-by: Stefan Lankes <slankes@eonerc.rwth-aachen.de>
    Co-authored-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/features.rs
+++ src/features.rs
@@ -372,6 +372,91 @@ pub mod console {
     impl crate::FeatureBits for F {}
 }
 
+pub mod blk {
+    use crate::le128;
+
+    feature_bits! {
+        /// Block Device Feature Bits
+        #[doc(alias = "VIRTIO_BLK_F")]
+        pub struct F: le128 {
+            /// Maximum size of any single segment is
+            // in `size_max`.
+            #[doc(alias = "VIRTIO_BLK_F_SIZE_MAX")]
+            const SIZE_MAX = 1 << 1;
+
+            /// Maximum number of segments in a
+            /// request is in `seg_max`.
+            #[doc(alias = "VIRTIO_BLK_F_SEG_MAX")]
+            const SEG_MAX = 1 << 2;
+
+            /// Disk-style geometry specified in
+            /// `geometry`.
+            #[doc(alias = "VIRTIO_BLK_F_GEOMETRY")]
+            const GEOMETRY = 1 << 4;
+
+            /// Device is read-only.
+            #[doc(alias = "VIRTIO_BLK_F_RO")]
+            const RO = 1 << 5;
+
+            /// Block size of disk is in `blk_size`.
+            #[doc(alias = "VIRTIO_BLK_F_BLK_SIZE")]
+            const BLK_SIZE = 1 << 6;
+
+            /// Cache flush command support.
+            #[doc(alias = "VIRTIO_BLK_F_FLUSH")]
+            const FLUSH = 1 << 9;
+
+            /// Device exports information on optimal I/O
+            /// alignment.
+            #[doc(alias = "VIRTIO_BLK_F_TOPOLOGY")]
+            const TOPOLOGY = 1 << 10;
+
+            /// Device can toggle its cache between writeback
+            /// and writethrough modes.
+            #[doc(alias = "VIRTIO_BLK_F_CONFIG_WCE")]
+            const CONFIG_WCE = 1 << 11;
+
+            /// Device supports multiqueue.
+            #[doc(alias = "VIRTIO_BLK_F_MQ")]
+            const MQ = 1 << 12;
+
+            /// Device can support discard command, maximum
+            /// discard sectors size in `max_discard_sectors` and maximum discard
+            /// segment number in `max_discard_seg`.
+            #[doc(alias = "VIRTIO_BLK_F_DISCARD")]
+            const DISCARD = 1 << 13;
+
+            /// Device can support write zeroes command,
+            /// maximum write zeroes sectors size in `max_write_zeroes_sectors` and
+            /// maximum write zeroes segment number in `max_write_zeroes_seg`.
+            #[doc(alias = "VIRTIO_BLK_F_WRITE_ZEROES")]
+            const WRITE_ZEROES = 1 << 14;
+
+            /// Device supports providing storage lifetime
+            /// information.
+            #[doc(alias = "VIRTIO_BLK_F_LIFETIME")]
+            const LIFETIME = 1 << 15;
+
+            /// Device supports secure erase command,
+            /// maximum erase sectors count in `max_secure_erase_sectors` and
+            /// maximum erase segment number in `max_secure_erase_seg`.
+            #[doc(alias = "VIRTIO_BLK_F_SECURE_ERASE")]
+            const SECURE_ERASE = 1 << 16;
+
+            /// Device is a Zoned Block Device, that is, a device
+            /// that follows the zoned storage device behavior that is also supported by
+            /// industry standards such as the T10 Zoned Block Command standard (ZBC r05) or
+            /// the NVMe(TM) NVM Express Zoned Namespace Command Set Specification 1.1b
+            /// (ZNS). For brevity, these standard documents are referred as "ZBD standards"
+            /// from this point on in the text.
+            #[doc(alias = "VIRTIO_BLK_F_ZONED")]
+            const ZONED = 1 << 17;
+        }
+    }
+
+    impl crate::FeatureBits for F {}
+}
+
 pub mod net {
     use crate::le128;
 
